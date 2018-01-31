a61de4b7f74c75ec2951681d7ac1ebf1ac6e3315
tomkru <tomasz.kruzel@msleopard.com>
2018-01-31 15:50:05 +0100

    test fix

--- app/src/test/java/com/example/timer/MainViewModelTest.java
+++ app/src/test/java/com/example/timer/MainViewModelTest.java
@@ -5,9 +5,10 @@ package com.example.timer;
  */
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule;
-
+import com.example.timer.sql.ScoreDAO;
+import com.example.timer.util.AppExecutors;
+import com.example.timer.util.TimeCounter;
 import com.example.timer.viewmodel.MainViewModel;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,26 +16,34 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by ignacy on 18.10.17.
  */
 @RunWith(MockitoJUnitRunner.class)
-public class MainActivityViewModelTest {
+public class MainViewModelTest {
 
-	MainViewModel viewModel;
+	private MainViewModel viewModel;
+	private TimeCounter timeCounter;
+	private ScoreDAO scoreDAO;
+	private AppExecutors executors;
 
 	@Before
 	public void setUp() {
-		//		viewModel = new MainActivityViewModel();
+		timeCounter = mock(TimeCounter.class);
+		scoreDAO = mock(ScoreDAO.class);
+		executors = mock(AppExecutors.class);
+		viewModel = new MainViewModel(timeCounter, scoreDAO, executors);
 	}
 
 	@Test
 	public void assertReturnsCounter() {
 		viewModel.startCounting();
 		assertThat(viewModel.getCounter()
-				.getValue(), is("counting"));
+				.getValue(), is(nullValue()));
 	}
 
 	@Rule
