1854f89ed5f32a5ed01a07d7f43af46a2220ea04
ignacy <ignacy.buczek@onet.pl>
2018-02-09 18:57:47 +0100

    added pager

--- app/src/main/java/com/example/timer/ui/main/MainActivity.java
+++ app/src/main/java/com/example/timer/ui/main/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.timer.ui.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.timer.R;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
@@ -18,17 +21,18 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
 
 	@Inject
 	DispatchingAndroidInjector<Fragment> fragmentInjector;
+	@BindView(R.id.pager)
+	ViewPager pager;
 
 	@Override
 	protected void onCreate(@Nullable final Bundle savedInstanceState) {
 		AndroidInjection.inject(this);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ButterKnife.bind(this);
 
 		if (null == savedInstanceState) {
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.frame, new CounterFragment())
-					.commitNow();
+			pager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
 		}
 
 	}
---
+++ app/src/main/java/com/example/timer/ui/main/MainPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.example.timer.ui.main;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by ignacy on 09.02.18.
+ */
+
+public class MainPagerAdapter extends FragmentPagerAdapter {
+
+	public MainPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		if (position == 0) {
+			return new CounterFragment();
+		}
+		return new ScoreFragment();
+
+	}
+
+	@Override
+	public int getCount() {
+		return 2;
+	}
+}
