af24a46b10964f09613882a5255ef8a728893370
ignacy <ignacy.buczek@onet.pl>
2018-04-19 17:26:48 +0200

    graphs work now

--- app/src/main/java/com/example/timer/businesslogic/timeprovider/GraphGeneratorImpl.java
+++ app/src/main/java/com/example/timer/businesslogic/timeprovider/GraphGeneratorImpl.java
@@ -1,6 +1,7 @@
 package com.example.timer.businesslogic.timeprovider;
 
 import com.example.timer.model.Statistics;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -16,10 +17,33 @@ public class GraphGeneratorImpl implements GraphGenerator {
 	@Override
 	public void makeGraph(GraphView graph, List<Statistics> values) {
 
-		LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] { new DataPoint(0, values.get(0)
-				.getSingle()), new DataPoint(1, values.get(1)
-				.getSingle()), });
+		DataPoint[] dataPoints;
+		dataPoints = new DataPoint[values.size()];
+		for (int i = 0; i < values.size(); i++) {
+			dataPoints[i] = new DataPoint(i, values.get(i)
+					.getSingle());
+		}
+
+		LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dataPoints);
+		series.setDrawDataPoints(true);
+		series.setDataPointsRadius(10f);
 		series.setAnimated(true);
 		graph.addSeries(series);
+		graph.getGridLabelRenderer()
+				.setLabelFormatter(new DefaultLabelFormatter() {
+
+					@Override
+					public String formatLabel(double value, boolean isValueX) {
+						if (isValueX) {
+							// show normal x values
+							return super.formatLabel(value, isValueX);
+						} else {
+							// show currency for y values
+							TimeFormatter formatter =
+									new TimeFormatterImpl(); // TODO próbowałem wstrzyknąć daggerem, ale coś nie pykło, dlatego jest tutaj
+							return formatter.formatTime((long) value);
+						}
+					}
+				});
 	}
 }
